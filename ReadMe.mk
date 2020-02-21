cd5278439f2c2305de0bae5d579d47b8216f68ab
Nathan Miller <nathanm@lanl.gov>
2020-02-20 21:02:35 -0700

    WIP: Initial commit of base files.

---
+++ ReadMe.mk
@@ -0,0 +1,9 @@
+# Micromorphic Tools
+
+author: Nathan Miller
+email: nathanm@lanl.gov
+
+A collection of useful tools and utilities for micromorphic continuum 
+mechanics. These utilities are intended to reduce the implementation time of 
+constitutive model development by providing a library of functions which are 
+verified and ready to be implemented.
---
+++ src/cpp/micromorphic_tools.h
@@ -0,0 +1,80 @@
+/*!
+ * Micromorphic Tools.h
+ * ====================
+ *
+ * A collection of tools and utilities which seek to make the 
+ * implementation of micromorphic continuum mechanics based constitutive 
+ * theories easier.
+ *
+ */
+
+#ifndef MICROMORPHIC_TOOLS_H
+#define MICROMORHPIC_TOOLS_H
+
+#include<error_tools.h>
+#include<vector_tools.h>
+#include<constitutive_tools.h>
+
+namespace micromorphicTools{
+
+    typedef double variableType;
+    typedef std::vector< variableType > variableVector;
+    typedef std::vector< variableVector > variableMatrix;
+
+    typedef double parameterType;
+    typedef std::vector< parameterType > parameterVector;
+    typedef std::vector< parameterVector > parameterMatrix;
+
+    errorOut computePsi( const variableVector &deformationGradient, const variableVector &microDeformation,
+		         variableVector &Psi );
+
+    errorOut computePsi( const variableVector &deformationGradient, const variableVector &microDeformation,
+		         variableVector &Psi, variableMatrix &dPsidF, variableMatrix &dPsidXi );
+
+    errorOut computeMicroStrain( const variableVector &Psi, variableVector &microStrain );
+
+    errorOut computeMicroStrain( const variableVector &Psi, variableVector &microStrain,
+		                 variableMatrix &dMicroStraindPsi );
+
+    errorOut pushForwardReferenceMicroStress( const variableVector &referenceMicroStress,
+		                              const variableVector &deformationGradient,
+					      variableVector &microStress );
+
+    errorOut pushForwardReferenceMicroStress( const variableVector &referenceMicroStress,
+		                              const variableVector &deformationGradient,
+					      variableVector &microStress, 
+                                              variableMatrix &dmicroStressdReferenceMicroStress,
+                                              variableMatrix &dmicroStressdDeformationGradient);
+
+    errorOut pushForwardHigherOrderStress( const variableVector &referenceHigherOrderStress,
+                                           const variableVector &deformationGradient,
+                                           const variableVector &microDeformation,
+                                           variableVector &higherOrderStress );
+
+    errorOut pushForwardHigherOrderStress( const variableVector &referenceHigherOrderStress,
+                                           const variableVector &deformationGradient,
+                                           const variableVector &microDeformation,
+                                           variableVector &higherOrderStress,
+                                           variableMatrix &dHigherOrderStressdReferenceHigherOrderStress,
+                                           variableMatrix &dHigherOrderStressdDeformationGradient,
+                                           variableMatrix &dHigherOrderStressdMicroDeformation );
+    
+    errorOut computeDeviatoricHigherOrderStress( const variableVector &higherOrderStress,
+                                                 variableVector &deviatoricHigherOrderStress );
+    
+    errorOut computeDeviatoricHigherOrderStress( const variableVector &higherOrderStress,
+                                                 variableVector &deviatoricHigherOrderStress,
+                                                 variableMatrix &dDeviatoricHigherOrderStressdHigherOrderStress);
+
+    errorOut computeDeviatoricReferenceHigherOrderStress( const variableVector &higherOrderStress,
+                                                          const variableVector &rightGreenLagrangeDeformation,
+                                                          variableVector &deviatoricHigherOrderStress );
+    
+    errorOut computeDeviatoricReferenceHigherOrderStress( const variableVector &higherOrderStress,
+                                                          const variableVector &rightGreenLagrangeDeformation,
+                                                          variableVector &deviatoricHigherOrderStress,
+                                                          variableMatrix &dDeviatoricHigherOrderStressdHigherOrderStress,
+                                                          variableMatrix &dDeviatoricReferenceHigherOrderStressdRCG );
+}
+
+#endif
