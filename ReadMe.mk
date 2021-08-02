a10b24238315d274d93789694c148e19c6c6f22a
Peter Mason Schaefferkoetter <peter.schaefferkoetter@lanl.gov>
2021-08-02 10:40:53 -0600

    WIP: corrected sytnax error

--- ReadMe.mk
+++
@@ -1,9 +0,0 @@
-# Micromorphic Tools
-
-author: Nathan Miller
-email: nathanm@lanl.gov
-
-A collection of useful tools and utilities for micromorphic continuum 
-mechanics. These utilities are intended to reduce the implementation time of 
-constitutive model development by providing a library of functions which are 
-verified and ready to be implemented.
